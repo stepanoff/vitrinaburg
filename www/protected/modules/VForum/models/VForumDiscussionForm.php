1edafe5b041658423cc12892634d0c1550209c1c
stepanoff <stenlex@gmail.com>
2012-06-12 18:04:00 +0600

    forum comment

---
+++ www/protected/modules/VForum/models/VForumDiscussionForm.php
@@ -0,0 +1,40 @@
+<?php
+class VDiscussionCommentForm extends CFormModel
+{
+    public $text;
+    public $parentId;
+
+    public function rules()
+    {
+        return array(
+            array('text', 'required', 'message'=>'Напишите что-нибудь' ),
+            array('text, parentId', 'safe'),
+		);
+    }
+
+    public function attributeLabels()
+    {
+        return array(
+                        'text' => 'Комментарий',
+                        'parentId' => 'Родительский комментарий',
+                    );
+    }
+
+    public function getElements ()
+    {
+        return array (
+            'elements' => array (
+                'text' => array (
+                    'type' => 'textarea',
+                )
+            ),
+            'buttons' => array (
+                'send'		 => array(
+                    'type' => 'submit',
+                    'label'=> 'Отправить',
+                ),
+            ),
+        );
+    }
+}
+?>

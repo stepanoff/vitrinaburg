32252309aa1bcb16cb58dd4fe64387c7dcf53b98
stepanoff <stenlex@gmail.com>
2012-11-05 21:00:17 +0600

    forget password

---
+++ www/protected/views/site/forgetPass.php
@@ -0,0 +1,6 @@
+<?php
+    echo '<h1>Добавить магазин</h1>';
+?>
+<div class="comment-form">
+    <?php $this->widget('application.extensions.VExtension.widgets.VFormBuilderWidget', array('form'=>$form)); ?>
+</div>

c511105b3b48b733a26eafb9a5c96cdf0cbb5d93
stepanoff <stenlex@gmail.com>
2012-11-18 22:17:07 +0600

    button widget

---
+++ www/protected/modules/VAdmin/widgets/VAdminButtonWidget.php
@@ -0,0 +1,9 @@
+<?php
+class VAdminGridWidget extends CGridView {
+
+    public $cssFile = false;
+    public $itemsCssClass = 'table table-striped table-hover';
+    public $rowCssClass = false;
+    public $rowCssClassExpression = null;
+    
+}
